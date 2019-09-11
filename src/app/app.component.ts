4ef521b74473d9731ff47eed9996e9815bef9842
michael-spengler <>
2019-09-11 17:33:46 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,34 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'richwood-frontend';
+  public title = 'DHBW Richie';
+  public learningmode = false;
+  public newInput = '';
+  public newOutput = '';
+  public trainingData = [
+    {
+      input: 'Wie bestehe ich immer die Klausur?',
+      output: 'Fleißig Lernen und üben'
+    },
+    {
+      input: 'Was sind die Schwerpunkte in der Datenbanken Klausur?',
+      output: 'Alles ist wichtig'
+    },
+    {
+      input: 'Was ist TSLint?',
+      output: 'TSLint ist ein Werkzeug für statische Code Checks.'
+    }
+  ];
+
+  public clickLernWasNeues() {
+    this.learningmode = true;
+  }
+
+  public clickSave() {
+    alert('Danke ...');
+  }
+
 }
