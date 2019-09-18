14f250e53ff61f1caa8206c5954fd3c4f242f88a
michael-spengler <>
2019-09-18 16:58:20 +0200

    https backend

--- src/app/backend-connector.service.ts
+++ src/app/backend-connector.service.ts
@@ -6,7 +6,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class BackendConnectorService {
 
-  private backendURL = 'http://95.217.10.3';
+  // private backendURL = 'http://95.217.10.3';
+  private backendURL = 'https://95.217.10.3';
   // private backendURL = 'http://localhost:3000';
   public constructor(private http: HttpClient) {
 
