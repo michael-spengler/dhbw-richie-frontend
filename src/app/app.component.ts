68fb821cc6f026ab7a02764132d49794d730489e
michael-spengler <>
2019-09-18 16:06:15 +0200

    adding training data works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,10 @@ export class AppComponent implements OnInit {
   }
 
   public clickSave() {
-    alert('Danke ...');
+
+    this.backendConnectorService.addTrainingData({input: this.newInput, output: this.newOutput })
+      .subscribe();
+
   }
 
 }
